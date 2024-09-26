15075422f68c7e5d152a42aec442027e48f6c1bf
clientesweb <alexisifran32@gmail.com>
2024-09-26 09:56:12 -0300

    Actualizar script.js

--- script.js
+++ script.js
@@ -7,7 +7,15 @@ $(document).ready(function() {
         fade: true,
         cssEase: 'linear',
         autoplay: true,
-        autoplaySpeed: 5000
+        autoplaySpeed: 5000,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: true
+                }
+            }
+        ]
     });
 
     // Mobile Menu Toggle
@@ -40,7 +48,7 @@ $(document).ready(function() {
     $('#newsletter-form').submit(function(e) {
         e.preventDefault();
         var email = $(this).find('input[type="email"]').val();
-        alert('Gracias por suscribirte con el email: ' + email);
+        alert('Thank you for subscribing with the email: ' + email);
         this.reset();
     });
 
@@ -59,6 +67,29 @@ $(document).ready(function() {
         if ($(window).width() > 768) {
             $('.main-nav').removeClass('active');
             $('.mobile-menu-toggle').removeClass('active');
+        }
+    });
+
+    // Lazy Loading Images
+    $('.lazy').Lazy({
+        scrollDirection: 'vertical',
+        effect: 'fadeIn',
+        visibleOnly: true,
+        onError: function(element) {
+            console.log('error loading ' + element.data('src'));
+        }
+    });
+
+    // Add to Cart Animation
+    $('.btn').on('click', function(e) {
+        if ($(this).text() === 'Add to Cart') {
+            e.preventDefault();
+            $(this).addClass('added');
+            $(this).text('Added to Cart');
+            setTimeout(() => {
+                $(this).removeClass('added');
+                $(this).text('Add to Cart');
+            }, 2000);
         }
     });
 });

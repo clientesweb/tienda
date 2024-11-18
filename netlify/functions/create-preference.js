d39461c72794eb303553527148ce3c2e6d73fa49
clientesweb <alexisifran32@gmail.com>
2024-11-18 02:25:22 -0300

    Actualizar create-preference.js

--- netlify/functions/create-preference.js
+++ netlify/functions/create-preference.js
@@ -5,22 +5,38 @@ exports.handler = async function(event, context) {
     return { statusCode: 405, body: 'Method Not Allowed' };
   }
 
-  const { items, shipments } = JSON.parse(event.body);
-
-  mercadopago.configure({
-    access_token: process.env.MERCADO_PAGO_ACCESS_TOKEN
-  });
-
   try {
+    const { items, payer } = JSON.parse(event.body);
+
+    mercadopago.configure({
+      access_token: process.env.MERCADOPAGO_ACCESS_TOKEN
+    });
+
     const preference = {
       items: items,
-      shipments: shipments
+      payer: payer,
+      back_urls: {
+        success: 'https://monamour.netlify.app/success',
+        failure: 'https://monamour.netlify.app/failure',
+        pending: 'https://monamour.netlify.app/pending'
+      },
+      auto_return: 'approved',
+      payment_methods: {
+        excluded_payment_types: [
+          { id: 'atm' }
+        ],
+        installments: 3
+      },
+      notification_url: 'https://monamour.netlify.app/.netlify/functions/webhook'
     };
 
     const response = await mercadopago.preferences.create(preference);
+
     return {
       statusCode: 200,
-      body: JSON.stringify({ id: response.body.id })
+      body: JSON.stringify({
+        id: response.body.id
+      })
     };
   } catch (error) {
     return {
